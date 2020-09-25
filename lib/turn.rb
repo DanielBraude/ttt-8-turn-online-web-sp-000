2b374e5b91aac97824766ea276877e20d88c1821
Daniel Abraham Braude <daniboy1007@gmail.com>
2020-09-25 19:17:01 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -11,9 +11,7 @@ def input_to_index(user_input)
 end
 
 def valid_move?(board, index)
-  if index.between?(0,8) && !position_taken?(board, index)
-  true
-end
+ index.between?(0,8) && !position_taken?(board, index)
 end
 
 def position_taken?(board, index)
